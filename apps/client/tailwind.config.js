7c1f12656fe7557f0dd62e257b5cbd7d543b0d3c
Boris Zubchenko <boriszubc@gmail.com>
2021-08-13 20:43:45 +0300

    tailwind.config.js initial config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,11 +1,24 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind')
+const { join } = require('path')
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  purge: [
+    join(__dirname, '**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
