c74a20a176f3b994bf9aea2ae69586ddb8123cb8
Boris Zubchenko <boriszubc@gmail.com>
2021-08-14 13:11:35 +0300

    jest

--- apps/client/jest.config.js
+++ apps/client/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   displayName: 'client',
   preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['./jest.setup.js'],
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   coverageDirectory: '../../coverage/apps/client',
 }
