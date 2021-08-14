79dde65014c15388510a61968c2c6498d838d441
Boris Zubchenko <boriszubc@gmail.com>
2021-08-14 13:11:58 +0300

    (preview.js) addDecorator

---
+++ apps/client/.storybook/preview.js
@@ -0,0 +1,6 @@
+import 'tailwindcss/tailwind.css'
+import { addDecorator } from '@storybook/react'
+import { withTests } from '@storybook/addon-jest'
+import results from '../.jest-test-results.json'
+
+addDecorator(withTests({ results }))
